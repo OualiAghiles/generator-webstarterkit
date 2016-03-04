f8e1e6f01135225016dd073e7414afe77471abbe
Allan Alexandre <alexandesigner@gmail.com>
2016-03-04 11:31:11 -0300

    (v0.0.5) Change de project core using Google Web Starter Kit

--- app/index.js
+++ app/index.js
@@ -3,9 +3,6 @@ var yeoman = require('yeoman-generator');
 var yosay = require('yosay');
 var chalk = require('chalk');
 var mkdirp = require('mkdirp');
-var taskRunners;
-var styleLang;
-var userInterface;
 
 module.exports = yeoman.generators.Base.extend({
 
@@ -20,109 +17,19 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have greet the user.
     this.log(yosay(
-      'Welcome, let\'s generate a ' + chalk.blue('WebStarterKit') + ' A front-end architeture generator that helps you build fast, modern mobile and browser web apps.'
+      'Welcome, let\'s generate a ' + chalk.blue('Web Starter Kit') + ' Scaffold out a mobile project that helps you build fast, modern mobile and browser web apps.'
     ));
 
-    // Questions
     var prompts = [{
       name: 'appName',
       message: 'What is your app\'s name ?',
-      default: 'WebStarterKit',
-    },
-    {
-      type: 'list',
-      name: 'taskRunner',
-      message: 'Would you like to use a Task-Runner for compiling?',
-      choices: [
-        {
-          name: 'Gulp',
-          value: 'gulp'
-        },
-        {
-          name: 'None',
-          value: 'none'
-        }
-      ]
-    },
-    {
-      type: 'list',
-      name: 'styleLang',
-      message: 'Which precompiler-language do you want to use?',
-      choices: [
-        {
-          name: 'SCSS',
-          value: 'Scss'
-        },
-        {
-          name: 'less',
-          value: 'Less'
-        },
-        {
-          name: 'None',
-          value: 'none'
-        }
-      ]
-    },
-    {
-      type: 'list',
-      name: 'chooseui',
-      message: 'Would you like to use a Framework?',
-      choices: [
-        {
-          name: 'UIKit',
-          value: 'uikit'
-        },
-        {
-          name: 'Bootstrap',
-          value: 'bootstrap'
-        },
-        {
-          name: 'Foundation',
-          value: 'foundation'
-        },
-        {
-          name: 'None',
-          value: 'none'
-        }
-      ]
+      default: 'Web Starter Kit',
     }];
 
     // Arguments Prompt
     this.prompt(prompts, function (props) {
 
-      this.appName               = props.appName;
-      userInterface                = props.chooseui;
-      taskRunners                  = props.taskRunner;
-      styleLang                        = props.styleLang;
-
-      this.useGulp                  = false;
-      this.useUIKit                 = false;
-      this.useBootstrap       = false;
-      this.useFoundation    = false;
-      this.Scss                         = false;
-      this.Less                         = false;
-
-      function wantsTaskrunner(tr) {
-        return taskRunners && taskRunners.indexOf(tr) !== -1;
-      }
-      function wantsFramework(fw) {
-        return userInterface && userInterface.indexOf(fw) !== -1;
-      }
-      function wantsStyleLang(sl) {
-        return styleLang && styleLang.indexOf(sl) !== -1;
-      }
-
-      // Task Runner Compiling
-      this.useGulp = wantsTaskrunner('gulp');
-
-      // Framework
-      this.useUIKit =  wantsFramework('uikit');
-      this.useBootstrap  = wantsFramework('bootstrap');
-      this.useFoundation =  wantsFramework('foundation');
-
-      // Styles
-      this.Scss = wantsStyleLang('Scss');
-      this.Less = wantsStyleLang('Less');
+      this.appName = props.appName;
 
       done();
     }.bind(this));
@@ -135,60 +42,18 @@ module.exports = yeoman.generators.Base.extend({
       this.template('_package.json', 'package.json');
     },
 
-    // App Context
-    app: function () {
-      var context = {
-          useUIKit: this.useUIKit,
-          useBootstrap: this.useBootstrap,
-          useFoundation: this.useFoundation,
-          Scss: this.Scss,
-          Less: this.Less
-      };
-      this.template('_bower.json', 'bower.json', context);
-    },
-
     // Files
     projectfiles: function () {
 
-      // Context Files
-      var context = {
-        appname: this.appName,
-        useUIKit: this.useUIKit,
-        useBootstrap: this.useBootstrap,
-        useFoundation: this.useFoundation,
-        appName: this.appName,
-        Scss: this.Scss,
-        Less: this.Less
-      };
-
-      // Editor Config
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-
-      // JSHint validate u javascript code
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-
-      // Bower dependencies
-      this.fs.copy(
-        this.templatePath('bowerrc'),
-        this.destinationPath('.bowerrc')
-      );
-
-      // Use Gulp
-      if (this.useGulp) {
-        this.template("_gulpfile.js", "gulpfile.js", context);
-      }
-
       // Create directories
       mkdirp('app');
-      mkdirp('src');
 
       // Scripts
+      this.fs.copy(
+        this.templatePath('_docs'),
+        this.destinationPath('docs')
+      );
+
       this.fs.copy(
         this.templatePath('app/_scripts'),
         this.destinationPath('app/scripts')
@@ -196,18 +61,9 @@ module.exports = yeoman.generators.Base.extend({
 
       // Styles
       this.directory(
-        this.templatePath('app/_stylesheets'),
-        this.destinationPath('app/stylesheets')
+        this.templatePath('app/_styles'),
+        this.destinationPath('app/styles')
       );
-
-      if (this.Scss) {
-        mkdirp('app/src/scss');
-        this.template('app/_src/scss', 'app/src/scss', context);
-        this.directory(
-          this.templatePath('app/_src/_scss'),
-          this.destinationPath('app/src/scss')
-        );
-      }
 
       // Images
       this.directory(
@@ -215,18 +71,37 @@ module.exports = yeoman.generators.Base.extend({
         this.destinationPath('app/images')
       );
 
-      // Fonts
-      this.directory(
-        this.templatePath('app/_fonts'),
-        this.destinationPath('app/fonts')
-      );
-
       // HTML
-      this.template("_index.html", "index.html", context);
+      this.template('app/_index.html', 'app/index.html');
+      this.template('app/_basic.html', 'app/basic.html');
+
+      // Favicon.ico
+      this.template('app/_favicon.ico', 'app/favicon.ico');
+
+      // Editor Config
+      this.template('editorconfig', '.editorconfig');
+
+      // JSHint validate u javascript code
+      this.template('babelrc', '.babelrc');
+
+      // Text Files
+      this.template('app/_humans.txt', 'app/humans.txt');
+      this.template('app/_robots.txt', 'app/robots.txt');
+
+      // Manifest
+      this.template('app/_manifest.webapp', 'app/manifest.webapp');
+      this.template('app/_manifest.json', 'app/manifest.json');
+
+      // Service Worker
+      this.template('app/_service-worker.js', 'app/service-worker.js');
+
+      // Gulp
+      this.template('_gulpfile.babel.js', 'gulpfile.babel.js');
+
     },
   },
 
-  // Run command install
+  //  Run command install
   install: function () {
 
     this.installDependencies({
@@ -234,13 +109,11 @@ module.exports = yeoman.generators.Base.extend({
     });
 
     this.on('end', function () {
-      if(taskRunners != 'none') {
         this.log(yosay(
           'Yeah! You\'re all set and done!' +
-          ' Now simply run ' + chalk.blue.italic('\''+taskRunners+'\'') + ' and start coding!'
+          ' Now simply run and start coding!'
         ));
         this.spawnCommand('gulp');
-      }
     });
 
   }
